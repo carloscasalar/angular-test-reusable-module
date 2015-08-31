5a1aba642e407bab6dd6037ad0f9a176a77b4e08
Carlos Castillo Alarcón <sir.ragnar@gmail.com>
2015-08-31 21:29:15 +0200

    Deleted temp files

--- temp/templates.js
+++
@@ -1,8 +0,0 @@
-angular.module('sirragnar.reusableModule').run(['$templateCache', function($templateCache) {
-  'use strict';
-
-  $templateCache.put('directive/rgtHelloWorld/rgtHelloWorld.html',
-    "<span>Hello world!</span>"
-  );
-
-}]);
